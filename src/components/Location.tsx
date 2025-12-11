913bba3e954a260cac8576bab739c5ef10cfbc0e
Arinze Ugwu <arinzmartcodes@gmail.com>
2025-12-11 17:50:53 +0100

    Update Map

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -39,7 +39,6 @@ const Location = () => {
           <div className="h-96 lg:h-[500px] rounded-lg overflow-hidden shadow-xl">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d876.4462448352256!2d8.532311258098636!3d7.719517558144474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1050810048818921%3A0x25e38912f51e47f1!2sCylux!5e1!3m2!1sen!2sus!4v1765471686683!5m2!1sen!2sus"
-              width="600"
               width="100%"
               height="100%"
               style={{ border: 0 }}
